eda59ce8bc917bf6908fa43bf36bab3b87668e2f
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-09-06 16:59:14 +0530

    minor changes html

--- legisense/lib/main.dart
+++ legisense/lib/main.dart
@@ -38,35 +38,6 @@ class _AppWrapperState extends State<AppWrapper> {
   bool isLoggedIn = false;
   int currentPageIndex = 0;
 
-  final List<GlobalKey<NavigatorState>> _navigatorKeys = List.generate(4, (_) => GlobalKey<NavigatorState>());
-
-  Widget _buildTabNavigator(int index) {
-    late final Widget rootPage;
-    switch (index) {
-      case 0:
-        rootPage = const HomePage();
-        break;
-      case 1:
-        rootPage = const DocumentsPage();
-        break;
-      case 2:
-        rootPage = const SimulationPage();
-        break;
-      case 3:
-        rootPage = ProfilePage(onLogout: handleLogout);
-        break;
-      default:
-        rootPage = const HomePage();
-    }
-
-    return Navigator(
-      key: _navigatorKeys[index],
-      onGenerateRoute: (settings) {
-        return MaterialPageRoute(builder: (_) => rootPage, settings: settings);
-      },
-    );
-  }
-
   void handleLoginSuccess() {
     setState(() {
       isLoggedIn = true;
@@ -94,26 +65,19 @@ class _AppWrapperState extends State<AppWrapper> {
       );
     }
 
-    final NavigatorState? currentNavigator = _navigatorKeys[currentPageIndex].currentState;
-    final bool nestedCanPop = currentNavigator?.canPop() ?? false;
-
-    return PopScope(
-      canPop: !nestedCanPop,
-      onPopInvokedWithResult: (didPop, result) {
-        if (didPop) return;
-        if (nestedCanPop) {
-          currentNavigator!.pop();
-        }
-      },
-      child: Scaffold(
-        body: IndexedStack(
-          index: currentPageIndex,
-          children: List.generate(4, _buildTabNavigator),
-        ),
-        bottomNavigationBar: BottomNavBar(
-          currentIndex: currentPageIndex,
-          onTap: onPageChanged,
-        ),
+    return Scaffold(
+      body: IndexedStack(
+        index: currentPageIndex,
+        children: [
+          const HomePage(),
+          const DocumentsPage(),
+          const SimulationPage(),
+          ProfilePage(onLogout: handleLogout),
+        ],
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: currentPageIndex,
+        onTap: onPageChanged,
       ),
     );
   }
