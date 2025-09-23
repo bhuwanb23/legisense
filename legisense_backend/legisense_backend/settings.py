778a13fcb29b8181331f5e2b5f861c1a056ba796
bhuwanb23 <bhuwanseervi4567@gmail.com>
2025-09-23 18:18:37 +0530

    hosting changes

--- legisense_backend/legisense_backend/settings.py
+++ legisense_backend/legisense_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,10 +84,11 @@ WSGI_APPLICATION = 'legisense_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://legisense_db_user:OxOoL6ySCYNOMFdvBuxYN8BjSHp8fSMP@dpg-d399baruibrs73abvjq0-a.singapore-postgres.render.com/legisense_db',
+        conn_max_age=600
+    )
 }
 
 
