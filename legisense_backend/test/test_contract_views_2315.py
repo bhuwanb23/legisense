25a3b33808dade95860fa64b8bef39b982873a65
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-04-23 14:32:45 +0000

    django-test: add contract views tests

---
+++ legisense_backend/test/test_contract_views_2315.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+
+class Testcontractviews(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_list(self):
+        response = self.client.get('/api/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create(self):
+        response = self.client.post('/api/', {})
+        self.assertIn(response.status_code, [200, 201, 400])
