638ae29ac40b28d7bdbf4a0b0fbaa4936b1ebd4f
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-08-01 19:38:54 +0530

    fix: add AppInsets import in risk dashboard page for improved layout consistency

--- app/lib/pages/analysis/risk_dashboard_page.dart
+++ app/lib/pages/analysis/risk_dashboard_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../data/analysis_mock.dart';
+import '../../theme/app_insets.dart';
 import '../../theme/app_theme.dart';
 import '../../widgets/analysis/risk_gauge.dart';
 import '../../widgets/analysis/risk_style.dart';
